e1044c261eeb19454a3b81f8ae57f0fa5cf84a64
Fred-H-Coding <69136202+Fred-H-Coding@users.noreply.github.com>
2025-09-02 17:25:45 -0400

    Complete assignment 07 of Javascript exercises

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringArray = string.split("");
+    let reversedStringArray = stringArray.reverse();
+    let reversedString = reversedStringArray.join("");
+    return reversedString;
 };
 
 // Do not edit below this line
