38a0fba9648b62ad07028c0cc5add47ea39d8f65
Fred-H-Coding <69136202+Fred-H-Coding@users.noreply.github.com>
2025-09-10 13:19:17 -0400

    Complete exercises 9,10,11. All tests passed.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
+const sumAll = function(x,y) {
+    let sumSpread = 0;
+    if (Number.isInteger(x) == false || Number.isInteger(y) == false || x < 0 || y < 0) {
+    return 'ERROR';
+    }
+    if (y >= x) {
+        for (let i = x; i <= y; i++) {
+            sumSpread += i;
+        }
 
+    }
+    else {
+        for (let i = y; i <= x; i++) {
+            sumSpread += i;
+        }
+    }
+    return sumSpread;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,8 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFahrenheit) {
+  let convertedTempC = (tempFahrenheit - 32)* (5/9);
+  convertedTempC = (Math.round(convertedTempC * 10))/10;
+  return convertedTempC;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCelsius) {
+  let convertedTempF = tempCelsius * (9/5) + 32;
+  convertedTempF = (Math.round(convertedTempF * 10))/10;
+  return convertedTempF;
 };
+
+
+// F = C * (9/5) + 32
+
+// C = (F - 32)(5/9)
 
 // Do not edit below this line
 module.exports = {
